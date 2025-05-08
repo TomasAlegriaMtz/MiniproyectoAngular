e467667f9510e5d6de47384cf353b23aaf1c60a7
JuanMontoya888 <jmaurixiomartinex@gmail.com>
2025-05-07 20:23:18 -0600

    Services Created

--- src/app/components/nuevos/nuevos.component.ts
+++ src/app/components/nuevos/nuevos.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ServicioNuevosAutosService } from './servicio-nuevos-autos.service';
 
 @Component({
   selector: 'app-nuevos',
@@ -7,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrl: './nuevos.component.css'
 })
 export class NuevosComponent {
+  //declaramos un array donde se almacenara el JSON
+  array: any = [];
 
+  //Inyectamos el servicio en este componente
+  constructor(public autosNuevosService: ServicioNuevosAutosService) {
+    //recuperaremos los datos en el constructor
+
+    this.autosNuevosService.getValues().subscribe({
+      next: this.successRequest.bind(this),
+      error: (err: any) => { console.log(err) }
+    });
+  }
+
+  successRequest(data: any): void {
+    console.log(data);
+    this.array = data.cars;
+    console.log(this.array);
+  }
 }
--- src/app/components/seminuevos/seminuevos.component.ts
+++ src/app/components/seminuevos/seminuevos.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ServicioAutosService } from './servicio-autos.service';
 
 @Component({
   selector: 'app-seminuevos',
@@ -7,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrl: './seminuevos.component.css'
 })
 export class SeminuevosComponent {
+  array: any = [];
 
+  //Inyectamos el servicio en este componente
+  constructor(public autosSeminuevosService: ServicioAutosService) {
+
+    //recuperamos los datos en el constructor
+    this.autosSeminuevosService.getValues().subscribe({
+      next: this.successRequest.bind(this),
+      error: (err: any) => { console.log(err) }
+    });
+
+  }
+
+  successRequest(data: any): void {
+    console.log(data);
+    this.array = data.cars;
+    console.log(this.array);
+  }
 }
