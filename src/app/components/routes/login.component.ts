d90b71a34d3c55baf1ce85ae762df47dbb250bdc
JuanMontoya888 <jmaurixiomartinex@gmail.com>
2025-05-09 10:50:33 -0600

    Login

--- src/app/components/routes/login.component.ts
+++ src/app/components/routes/login.component.ts
@@ -1,50 +1,64 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, FormsModule, FormGroup, Validators } from '@angular/forms';
 import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  user!: { username: string, password: string };
+  //user!: { username: string, password: string };
+  formulario!: FormGroup;
+  accounts!: { username: string, password: string }[];
 
-  constructor() { this.user = { username: '', password: '' } }
-
-  login(): void {
+  constructor(private formBuilder: FormBuilder) {
     const accountsRaw = localStorage.getItem('accounts');
-    let acc: { username: string, password: string }[] = accountsRaw ? JSON.parse(accountsRaw) : [];
+    this.accounts = accountsRaw ? JSON.parse(accountsRaw) : [];
 
-    console.log(this.user);
-
-    if (acc.length > 0) {
-      const match = acc.find(us => us.username === this.user.username && us.password === this.user.password);
-      if (match) {
-        Swal.fire({
-          icon: 'success',
-          title: '¡Acceso concedido!',
-          text: `Bienvenido, ${match.username}`,
-        });
-      } else {
-        Swal.fire({
-          icon: 'error',
-          title: 'Error de autenticación',
-          text: 'Usuario o contraseña incorrectos',
-        });
-
-        this.user = { username: '', password: '' };
-      }
-
-    } else {
+    //Ingresamos al localStorage si es que no existen
+    if (this.accounts.length < 1) {
       const defaultAccounts = [
         { username: 'Admin01', password: 'password_admin01' },
         { username: 'Admin02', password: 'password_admin02' },
         { username: 'Admin03', password: 'password_admin03' }
       ];
-
       localStorage.setItem('accounts', JSON.stringify(defaultAccounts));
+      this.accounts = defaultAccounts;
     }
+    console.log(this.accounts);
   }
+
+  ngOnInit(): void {
+    this.formulario = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  //ahora revisamos el login
+  login(): void {
+    let acc = this.accounts.find(us => us.username === this.formulario.get('username')?.value
+      && us.password === this.formulario.get('password')?.value);
+
+    console.log(acc);
+
+    if (acc) {
+      Swal.fire({
+        icon: 'success',
+        title: '¡Acceso concedido!',
+        text: `Bienvenido, ${acc.username}`,
+      });
+    } else {
+      Swal.fire({
+        icon: 'error',
+        title: 'Error de autenticación',
+        text: 'Usuario o contraseña incorrectos',
+      });
+      this.formulario.reset();
+    }
+
+  }
+
 }
